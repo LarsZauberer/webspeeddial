0e9991657696d372db7272a628d905d3df712007
LarsZauberer <wasser.ian@gmail.com>
2025-03-17 17:24:14 +0100

    xdg-opener

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,16 @@
+use std::process::{Command, Stdio};
+
 fn main() {
 }
+
+fn open_file(url: &str) {
+    let mut xdg = Command::new("xdg-open");
+    xdg.arg(url);
+    let spawning_result = xdg.spawn();
+    if spawning_result.is_err() {
+        println!("{:?}", spawning_result.unwrap_err());
+        return;
+    }
+    let _ = spawning_result.unwrap().wait();
+}
+
