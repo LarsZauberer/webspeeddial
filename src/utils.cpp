89be1265abd127ccc95e0b6f18dfc5231061e320
LarsZauberer <wasser.ian@gmail.com>
2025-03-20 16:02:54 +0100

    remove_trailing added

--- src/utils.cpp
+++ src/utils.cpp
@@ -43,3 +43,18 @@ BookMark *find_name(string *name, BookMark **arr, size_t n) {
   return NULL;
 }
 
+string *remove_trailing(string *str) {
+    if (!str) {
+        return NULL;
+    }
+    size_t pos = (*str).rfind("\n");
+
+    if (pos == ((size_t) -1)) {
+        // No match found
+        return str;
+    }
+
+    (*str).erase(pos);
+
+    return str;
+}
