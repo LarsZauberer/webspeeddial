a16cd24e227ab8001f5de625749be0e5cfbfbd8c
LarsZauberer <wasser.ian@gmail.com>
2025-03-20 16:01:34 +0100

    Added fzf_bookmark string generator

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,5 +1,27 @@
+#include "utils.h"
 #include "config.h"
+#include <cstddef>
+#include <string>
 
-char* getSelection(BookMark* bookmarks) {
-    return (char*) "";
+using std::string;
+
+string bookmarks_to_fzf(BookMark **bookmarks, size_t n) {
+  if (!bookmarks) {
+    return "";
+  }
+  string res = "";
+  for (size_t i = 0; i < n; i++) {
+    if (!*bookmarks)
+      break;
+
+    if (i == n - 1) {
+      // Last element
+      res += bookmarks[i]->name;
+      continue;
+    }
+
+    res += bookmarks[i]->name + "\n";
+  }
+
+  return res;
 }
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -13,3 +13,50 @@ TEST_CASE( "Factorials are computed", "[factorial]" ) {
     REQUIRE( factorial( 3) == 6 );
     REQUIRE( factorial(10) == 3'628'800 );
 }
+
+TEST_CASE("Find BookMark null array", "[find_name]") {
+  BookMark **bookmarks = NULL;
+  string find = "Hello";
+  REQUIRE(find_name(&find, bookmarks, 0) == NULL);
+}
+
+TEST_CASE("Find BookMark null element", "[find_name]") {
+  BookMark a = {"Hello", "1"};
+  BookMark b = {"World", "2"};
+  BookMark *bookmarks[] = {&a, &b};
+  string *find = NULL;
+  REQUIRE(find_name(find, bookmarks, 2) == NULL);
+}
+
+TEST_CASE("Find BookMark first_ele", "[find_name]") {
+  BookMark a = {"Hello", "1"};
+  BookMark b = {"World", "2"};
+  BookMark *bookmarks[] = {&a, &b};
+  string find = "Hello";
+  REQUIRE(find_name(&find, bookmarks, 2) == &a);
+}
+
+TEST_CASE("Find BookMark second_ele", "[find_name]") {
+  BookMark a = {"Hello", "1"};
+  BookMark b = {"World", "2"};
+  BookMark *bookmarks[] = {&a, &b};
+  string find = "World";
+  REQUIRE(find_name(&find, bookmarks, 2) == &b);
+}
+
+TEST_CASE("Remove Trailing Null", "[remove_trailing]") {
+  string *a = NULL;
+  REQUIRE(remove_trailing(a) == NULL);
+}
+
+TEST_CASE("Remove Trailing No backslash n", "[remove_trailing]") {
+  string a = "test";
+  REQUIRE(*remove_trailing(&a) == a);
+  REQUIRE(remove_trailing(&a) == &a);
+}
+
+TEST_CASE("Remove Trailing with backslash n", "[remove_trailing]") {
+  string a = "test\n";
+  REQUIRE(*remove_trailing(&a) == "test");
+  REQUIRE(remove_trailing(&a) == &a);
+}
