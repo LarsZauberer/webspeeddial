da5605f63027a3f24f661eb3293347da9f544c70
LarsZauberer <wasser.ian@gmail.com>
2025-03-21 08:22:12 +0100

    formated commands.h

--- src/commands.h
+++ src/commands.h
@@ -2,7 +2,8 @@
 #define commands_h_INCLUDED
 
 #include <string>
-std::string runner(std::string* runner, std::string* data);
-void xdg_open(std::string* link);
+
+std::string runner(std::string *runner, std::string *data);
+void xdg_open(std::string *link);
 
 #endif // commands_h_INCLUDED
