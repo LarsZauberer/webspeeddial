04f05e5ef76c56225692e918a18fc63d2fa17bd0
LarsZauberer <wasser.ian@gmail.com>
2025-03-29 20:03:43 +0100

    Merge utils and commands

--- include/webspeeddial/utils.h
+++ include/webspeeddial/utils.h
@@ -80,6 +80,36 @@ BookMark *find_name(std::string *name, T *arr) {
  */
 std::string *remove_trailing(std::string *str);
 
+/**@brief Returns the content of a `FILE*` descriptor as a string
+ * @param f A pointer to a `FILE`
+ * @return A string with the content of `f`
+ * @note The string needs to be deallocated
+ */
+std::string *copy_content_from_file(FILE *f);
+
+/**@brief Runs a command and returns the file descriptor
+ * @tparam T A `CMD_Runner`
+ * @param runner A pointer to a `CMD_Runner`
+ * @return A file descriptor to the command run
+ * @see CMD_Runner
+ * @note The string needs to be deallocated
+ */
+template <CMD_Runner T> std::string *run_cmd(T *runner) {
+  // NULL Checker
+  if (!runner) {
+    return NULL;
+  }
+
+  FILE *f = runner->run();
+  std::string *out = copy_content_from_file(f);
+
+  // Cleanup
+  runner->close();
+  f = NULL;
+
+  return out;
+};
+
 }; // namespace core
 
 #endif // utils_h_INCLUDED
--- src/core/utils.cpp
+++ src/core/utils.cpp
@@ -3,19 +3,33 @@
 namespace core {
 
 std::string *remove_trailing(std::string *str) {
-    if (!str) {
-        return NULL;
-    }
-    size_t pos = (*str).rfind("\n");
+  if (!str) {
+    return NULL;
+  }
+  size_t pos = (*str).rfind("\n");
 
-    if (pos == ((size_t) -1)) {
-        // No match found
-        return str;
-    }
-
-    (*str).erase(pos);
-
+  if (pos == ((size_t)-1)) {
+    // No match found
     return str;
-} 
+  }
 
-};
+  (*str).erase(pos);
+
+  return str;
+}
+
+std::string *copy_content_from_file(FILE *f) {
+  if (!f) {
+    return NULL;
+  }
+  // Read stdout from the file
+  std::string *out = new std::string("");
+  int c;
+  while ((c = fgetc(f)) != -1) {
+    *out += (char)c;
+  }
+
+  return out;
+}
+
+}; // namespace core
